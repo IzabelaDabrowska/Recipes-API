7eadbe8adf59587f261bf386878df218ef26b2f1
Izabela Dąbrowska <izabeladabrowska@Izabelas-MacBook-Pro.local>
2024-01-04 09:53:05 +0100

    set time for tokens

--- recipes_api/settings.py
+++ recipes_api/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
+from datetime import timedelta
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -93,6 +94,8 @@ REST_FRAMEWORK = {
 
 # simple jwt - konfigurujemy libke
 SIMPLE_JWT = {
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=60),
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
     'AUTH_HEADER_TYPES': [
         'JWT',
         'Bearer'
