e049e40ec1cbf81a41e15268ca9a51015fede4e9
Izabela Dąbrowska <izabeladabrowska@Izabelas-MacBook-Pro.local>
2024-01-21 17:47:17 +0100

    added image to category

--- categories/models.py
+++ categories/models.py
@@ -3,6 +3,7 @@ from django.db import models
 
 class Category(models.Model):
     name = models.CharField(max_length=20)
+    image = models.ImageField(blank=True, null=True)
 
     def __str__(self) -> str:
         return self.name
